3405d0053aa19ea6f324d9c08b2a0be39c813946
Anand Sharma <anand.sharma@resourcifi.com>
2022-06-24 00:40:52 +0530

    shop

--- src/components/Layout/NavigationLink.jsx
+++ src/components/Layout/NavigationLink.jsx
@@ -5,16 +5,15 @@ const NavigationLink = (props) => {
   const location = useLocation();
   const currentPath = location.pathname;
   return (
-    <Link to={props.href}>
-      <a
-        className={
-          currentPath == props.href
-            ? "nav-item nav-link active"
-            : "nav-item nav-link"
-        }
-      >
-        {props.children}
-      </a>
+    <Link
+      to={props.href}
+      className={
+        currentPath == props.href
+          ? "nav-item nav-link active"
+          : "nav-item nav-link"
+      }
+    >
+      {props.children}
     </Link>
   );
 };
--- src/pages/Shop/index.jsx
+++ src/pages/Shop/index.jsx
@@ -1,23 +1,94 @@
 import React from "react";
 import Layout from "../../components/Layout";
 import PageHeader from "../../components/Layout/PageHeader";
+import ColorFilter from "../../components/Shop/ColorFilter";
+import Pagination from "../../components/Shop/Pagination";
+import PriceFilter from "../../components/Shop/PriceFilter";
+import Product from "../../components/Shop/Product";
+import SizeFilter from "../../components/Shop/SizeFilter";
 
 const Shop = () => {
   return (
-    <Layout>
-      <PageHeader
-        title="Our Shop"
-        breadcrumb={[
-          {
-            name: "Home",
-            href: "/",
-          },
-          {
-            name: "Shop",
-          },
-        ]}
-      />
-    </Layout>
+    <>
+      <Layout>
+        <PageHeader
+          title="Our Shop"
+          breadcrumb={[
+            {
+              name: "Home",
+              href: "/",
+            },
+            {
+              name: "Shop",
+            },
+          ]}
+        />
+        <div className="container-fluid pt-5">
+          <div className="row px-xl-5">
+            <div className="col-lg-3 col-md-12">
+              <PriceFilter />
+              <ColorFilter />
+              <SizeFilter />
+            </div>
+            <div className="col-lg-9 col-md-12">
+              <div className="row pb-3">
+                <div className="col-12 pb-1">
+                  <div className="d-flex align-items-center justify-content-between mb-4">
+                    <form action="">
+                      <div className="input-group">
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Search by name"
+                        />
+                        <div className="input-group-append">
+                          <span className="input-group-text bg-transparent text-primary">
+                            <i className="fa fa-search"></i>
+                          </span>
+                        </div>
+                      </div>
+                    </form>
+                    <div className="dropdown ml-4">
+                      <button
+                        className="btn border dropdown-toggle"
+                        type="button"
+                        id="triggerId"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                      >
+                        Sort by
+                      </button>
+                      <div
+                        className="dropdown-menu dropdown-menu-right"
+                        aria-labelledby="triggerId"
+                      >
+                        <a className="dropdown-item" href="#">
+                          Latest
+                        </a>
+                        <a className="dropdown-item" href="#">
+                          Popularity
+                        </a>
+                        <a className="dropdown-item" href="#">
+                          Best Rating
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Pagination />
+              </div>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    </>
   );
 };
 
