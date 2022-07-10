271ecedc63ac1195737744c16f2225e33bf59c9b
Anand Sharma <anand.sharma@resourcifi.com>
2022-07-11 00:56:37 +0530

    Quantity update

--- src/pages/Product/AddToCart.jsx
+++ src/pages/Product/AddToCart.jsx
@@ -1,21 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddToCart = () => {
+  const [quantity, setQuantity] = useState(1);
+
+  const updateQuantity = (action) => {
+    if (action == "add") {
+      setQuantity((prevState) => prevState + 1);
+    } else {
+      setQuantity((prevState) => {
+        if (prevState != 1) {
+          return prevState - 1;
+        }
+        return prevState;
+      });
+    }
+  };
   return (
     <div className="d-flex align-items-center mb-4 pt-2">
       <div className="input-group quantity mr-3" style={{ width: "130px" }}>
         <div className="input-group-btn">
-          <button className="btn btn-primary btn-minus">
+          <button
+            className="btn btn-primary btn-minus"
+            onClick={() => updateQuantity("sub")}
+          >
             <i className="fa fa-minus"></i>
           </button>
         </div>
         <input
           type="text"
           className="form-control bg-secondary text-center"
-          defaultValue="1"
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
         />
         <div className="input-group-btn">
-          <button className="btn btn-primary btn-plus">
+          <button
+            className="btn btn-primary btn-plus"
+            onClick={() => updateQuantity("add")}
+          >
             <i className="fa fa-plus"></i>
           </button>
         </div>
